ec1a347c584abd227174f50ff4462a9a55396dd3
RyanFlageolle <rflageol@msudenver.edu>
2018-09-23 10:20:36 -0600

    got the models interacting correctly, with the courses being shown correctly

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+classes = [
+  [  'Introduction to Theory of Computation',   'CS',   52566,   2],
+  [  'Software Development Methods and Tools',   'CS',   54612,   4],
+  [  'Web Application Architecture',   'CS',   56028,   4],
+  [  'Probability and Statistics',   'MTH',   50330,   4]
+]
+
+classes.each do |name, department, number, credit_hours|
+  Course.create(name: name, department: department, number: number, credit_hours: credit_hours)
+end
